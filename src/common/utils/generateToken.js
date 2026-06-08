18e52b73336dbb7a993a7e2dd6d52c6b887accec
Ahmed Abd El Nasser <ahmedcomputerscience325@gmail.com>
2026-06-08 21:58:24 +0300

    Delete src/common/utils/generateToken.js

--- src/common/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import { env } from "../../config/env.service.js";
-export const tokenGnerate = (userId, userRole) => {
-  const token = jwt.sign({ id: userId, role: userRole }, env.tokenPassword, {
-    expiresIn: "7d",
-  });
-  return token;
-};
